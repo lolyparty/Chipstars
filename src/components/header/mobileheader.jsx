d020b5eaea8423110a2479c695b10efaf6644fad
lolyparty <ololadeolayiwola01@gmail.com>
2022-04-13 06:59:02 +0100

    updated UK flag

--- src/components/header/mobileheader.jsx
+++ src/components/header/mobileheader.jsx
@@ -1,15 +1,16 @@
 import Box from '@mui/material/Box';
 import MenuIcon from '@mui/icons-material/Menu';
 import { LoginRegisterBtn } from "./LoginregisterBtn"
+import ukFlag from '../Imgs/UK-flag.svg'
 
 const Mobileheader = ({matches739}) =>{
     return <Box sx={{alignItems:'center', display:'flex', justifyContent:'space-between', width:'100%', padding:'5px'}}>
         <MenuIcon sx={{marginLeft:'5px'}}/>
         <div style={{display:'flex', gap:'8px', alignItems:'center'}}>
-            <LoginRegisterBtn matches739={matches739} style={{display:'inline-block'}}/>
-            <div style={{display:'inline-block'}}>Flag</div>
+            <LoginRegisterBtn matches739={matches739}/>
+            <img src={ukFlag} width={20} height={20} style={{marginLeft:'10px'}} alt="Flag of the United Kingdom"/>
         </div>
-    </Box>
+    </Box> 
 }
 
 export {Mobileheader}
